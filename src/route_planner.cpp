4a0eba17f522f95db0f23888bb0a0579cfef4a57
vfxnake <jazzjoe@gmail.com>
2022-09-13 22:41:25 -0400

    extra tips for todo 4

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -21,7 +21,7 @@ float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
 // TODO 4: Complete the AddNeighbors method to expand the current node by adding all unvisited neighbors to the open list.
 // Tips:
 // - Use the FindNeighbors() method of the current_node to populate current_node.neighbors vector with all the neighbors.
-// - For each node in current_node.neighbors, set the parent, the h_value, the g_value. 
+// - For each node in current_node.neighbors, set the parent(current_node), the h_value (calculate h), the g_value (g+ distance()). 
 // - Use CalculateHValue below to implement the h-Value calculation.
 // - For each node in current_node.neighbors, add the neighbor to open_list and set the node's visited attribute to true.
 
